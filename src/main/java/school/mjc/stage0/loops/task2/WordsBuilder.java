8bf05ab24627244424a57c8839f924d1cd7573cd
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-09 22:03:46 +0300

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+   String s = "";
+        int i = 0;
+        while (i < chars.length && chars[i] != ' ') {
+            s = s + chars[i];
+        }
+        System.out.println(s);
     }
 }
